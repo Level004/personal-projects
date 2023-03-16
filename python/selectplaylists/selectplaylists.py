d995a36d5491751c49ce38c9c445d0079244d364
Guilliano van Dinther <guilliano.vandinther@ictmbo.nl>
2023-03-16 14:57:40 +0100

    changed some values & it now also shuffles after it's done with the set up

--- python/selectplaylists/selectplaylists.py
+++ python/selectplaylists/selectplaylists.py
@@ -8,18 +8,18 @@ webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chromePath))
 
 # opens up chrome and goes to the url
 webbrowser.get('chrome').open_new_tab(urL)
-pyautogui.sleep(3)
+pyautogui.sleep(4)
 
 # clicks on the top library track
-pyautogui.click(164, 391)
+pyautogui.click(197, 339)
 
 # this has to be done because YT music doesn't use all the tracks in the library
 # moves to the bottom of the list and makes sure that every song is loaded
 pyautogui.moveTo(1255, 942)
-pyautogui.sleep(2.5)
+pyautogui.sleep(3)
 pyautogui.scroll(-2000)
 pyautogui.click()
-pyautogui.sleep(2.5)
+pyautogui.sleep(3)
 pyautogui.scroll(-4000)
 pyautogui.click()
 
@@ -33,3 +33,5 @@ pyautogui.rightClick(405, 414)
 pyautogui.sleep(0.5)
 pyautogui.click(490, 592)
 pyautogui.hotkey('q')
+pyautogui.sleep(0.5)
+pyautogui.hotkey('s')
