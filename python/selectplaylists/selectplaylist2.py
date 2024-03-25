481aa1c24cbeb120173e64565aa6e017d3f980ca
Guilliano van Dinther <guilliano.vandinther@ictmbo.nl>
2024-03-25 14:58:56 +0100

    WIP

--- python/selectplaylists/selectplaylist2.py
+++ python/selectplaylists/selectplaylist2.py
@@ -1,23 +1,62 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+import pychrome
+from time import sleep
 
-# Specify the URL of the Chrome DevTools Protocol server
-chrome_devtools_url = "http://localhost:9222"
+# Define a flag to indicate whether the page has loaded
+page_loaded = False
 
-# Specify the path to the Chrome driver executable
-chrome_driver_path = "C:/Users/guill/Documents/chromedriver-win64/chromedriver.exe"
+# Define a callback function to handle the load event
+def load_event_handler(**kwargs):
+    global page_loaded
+    page_loaded = True
 
-# Create a Chrome service object with the specified URL and executable path
-chrome_service = Service(executable_path=chrome_driver_path)
+# Create a new browser instance
+browser = pychrome.Browser(url="http://127.0.0.1:9222")
 
-# Connect to the existing Chrome instance using the Chrome service
-browser = webdriver.Chrome(service=chrome_service)
+# Create a new tab
+tab = browser.new_tab()
 
-# Now you can interact with the browser as needed
-browser.get('https://www.google.com')
+# Start the tab
+tab.start()
 
-# Keep the browser open for demonstration purposes
-input("Press Enter to close the browser...")
+# Enable required domains
+tab.call_method("Page.enable")
+tab.call_method("Runtime.enable")
 
-# # Close the browser
-# browser.quit()
+# Subscribe to the load event
+tab.set_listener("Page.loadEventFired", load_event_handler)
+
+# Navigate to the specified URL
+tab.call_method("Page.navigate", url="https://music.youtube.com/library/songs")
+
+# Wait for the page to load
+while not page_loaded:
+    pass
+
+# Execute JavaScript code to click on the element with id "play-button" inside the last child element
+click_play_button_script = """
+    var parentElement = document.querySelector('#contents > ytmusic-responsive-list-item-renderer:nth-child(14)');
+    if (parentElement) {
+        var playButton = parentElement.querySelector('#play-button');
+        if (playButton) {
+            playButton.click();
+        }
+    }
+"""
+tab.call_method("Runtime.evaluate", expression=click_play_button_script)
+
+# Wait for a brief moment for the next page to load
+sleep(1)
+
+# Execute JavaScript code to click on the element with id "play-button" inside the last child element on the new page
+click_play_button_script = """
+    var lastChildElement = document.querySelector('#contents > ytmusic-player-queue-item:last-child');
+    if (lastChildElement) {
+        var playButton = lastChildElement.querySelector('#play-button');
+        if (playButton) {
+            playButton.click();
+        }
+    }
+"""
+tab.call_method("Runtime.evaluate", expression=click_play_button_script)
+
+# Don't close the tab or browser
